15db8329e29b8c3d615510e6ea981c0ce673b369
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-04-27 17:15:50 +0200

    try

---
+++ components/NoteForm/NoteForm.tsx
@@ -0,0 +1,77 @@
+"use client"
+import css from './NoteForm.module.css';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createNote } from "@/lib/api";
+import type {NoteFormValues} from '@/types/note'
+
+const validationSchema = Yup.object({
+  title: Yup.string().min(3).max(50).required(),
+  content: Yup.string().max(500),
+  tag: Yup.string().oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"]).required(),
+});
+type NoteFormProps = {
+  onClose: () => void;
+};
+export default function NoteForm({ onClose }:NoteFormProps) {
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"]});
+      onClose();
+    },
+  });
+
+  const formik = useFormik<NoteFormValues>({
+    initialValues: {
+      title: "",
+      content: "",
+      tag: "Todo",
+    },
+    validationSchema,
+    onSubmit: (values ) => {
+      mutation.mutate(values);
+    },
+  });
+
+  return (
+          <form className={css.form} onSubmit={formik.handleSubmit}>
+
+            <div className={css.formGroup}>
+              <label htmlFor="title">Title</label>
+              <input id="title" name="title" className={css.input} value={formik.values.title} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              {formik.touched.title && formik.errors.title && (
+                <span className={css.error}>{formik.errors.title}</span>
+              )}
+            </div>
+            
+            <div className={css.formGroup}>
+              <label htmlFor="content">Content</label>
+              <textarea id="content" name="content" className={css.textarea} value={formik.values.content} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+              {formik.touched.content && formik.errors.content && (<span className={css.error}>{formik.errors.content}</span>)}
+            </div>
+            
+            <div className={css.formGroup}>
+              <label htmlFor="tag">Tag</label>
+              <select id="tag" name="tag" className={css.select} value={formik.values.tag} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+                <option value="Todo">Todo</option>
+                <option value="Work">Work</option>
+                <option value="Personal">Personal</option>
+                <option value="Meeting">Meeting</option>
+                <option value="Shopping">Shopping</option>
+              </select>
+              {formik.touched.tag && formik.errors.tag && (<span className={css.error}>{formik.errors.tag}</span>)}
+            </div>
+            
+            <div className={css.actions}>
+              <button type="button" className={css.cancelButton} onClick={onClose}>Cancel</button>
+            
+              <button type="submit" className={css.submitButton} disabled={mutation.isPending}>Create note</button>
+            </div>
+          </form>
+  );
+}
