cab8eacd838eca84e854b2d40e128d0bb81bb2d4
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-04-27 17:48:09 +0200

    try 3

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -2,19 +2,13 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import { useParams } from "next/navigation";
 import css from "./NoteDetails.module.css";
 
-export default function NoteDetailsClient() {
-  const params = useParams();
-  const id = params.id;
-
+export default function NoteDetailsClient({ id }: { id: string }) {
   const { data: note, isLoading, error } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
   });
-
-
   if (isLoading) return <p>Loading, please wait...</p>;
   if (error || !note) return <p>Something went wrong.</p>;
 
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,17 +2,14 @@ import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-export default async function NoteDetailsPage({ params }: { params: { id: string } }) {
+export default async function NoteDetailsPage({params,}: {params: Promise<{ id: string }>}){
+
+  const { id } = await params;
   const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["note", params.id],
-    queryFn: () => fetchNoteById(params.id),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient />
-    </HydrationBoundary>
-  );
-}
+  await queryClient.prefetchQuery({ queryKey: ["note", id],
+     queryFn: () => fetchNoteById(id),
+ });
+  return ( 
+  <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id}/> 
+     </HydrationBoundary> ); }
